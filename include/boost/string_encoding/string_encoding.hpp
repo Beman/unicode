598295ac8d43c0a99641e52436140b54dc2e940b
Beman <bdawes@acm.org>
2015-06-21 15:47:15 -0400

    Add more structure and preliminary code for codecvt based conversions. Remove more development residue.

--- include/boost/string_encoding/string_encoding.hpp
+++ include/boost/string_encoding/string_encoding.hpp
@@ -335,14 +335,65 @@ namespace string_encoding
 
     template <class InputIterator, class OutputIterator /*, class Error*/> inline
     OutputIterator recode_codecvt(narrow, wide,
-      InputIterator first, InputIterator last, OutputIterator result /*, Error eh*/)
+      InputIterator first, InputIterator last, OutputIterator result,
+      const codecvt_type& ccvt /*, Error eh*/)
     {
       cout << "  narrow to wide" << endl;
+      constexpr std::size_t buf_size = 20;  // TODO increase this after initial testing
+      char in[buf_size];
+      const char* in_end = &in[0] + buf_size;
+      wchar_t out[buf_size];
+      std::mbstate_t mbstate  = std::mbstate_t();
+      wchar_t* out_next;
+
+      while (first != last)
+      {
+        //  fill the in buffer
+        char* in_last = &in[0];
+        for (; first != last && in_last != in_end; ++in_last, ++first)
+          *in_last = *first;
+
+        //  loop until the entire input buffer is processed by the codecvt facet; 
+        //  required because the codecvt facet will not process the entire input sequence
+        //  when an error occurs or the in buffer ended with only a portion of a multibyte
+        //  character.
+        char* in_first = &in[0];
+        for (; in_first != in_last;)
+        {
+          //  convert in buffer to out buffer
+          const char* in_next;
+          std::codecvt_base::result cvt_result
+            = cvt.in(mbstate, in, in_last, in_next, out, out + buf_size, out_next);
+
+          BOOST_ASSERT(cvt_result != std::codecvt_base::noconv);
+
+          if (cvt_result == std::codecvt_base::error)
+          {
+            //BOOST_ASSERT_MSG(false, "error handling not implemented yet")
+            wchar_t placeholder = eh()
+          }
+
+          //  Note: it is not necessary to further distinguish between an error, partial,
+          //  or ok result. The distinctiion is already captured in the mbstate.
+
+          //  copy out buffer to result
+          for (const wchar_t* itr = &out[0]; itr != out_next; ++itr, ++result)
+            *result = *itr;
+
+          in_first = in_next;
+        }
+
+        //  process cvt_result ok, partial, error
+
+      }
+
+      return result;
     }
 
     template <class InputIterator, class OutputIterator /*, class Error*/> inline
     OutputIterator recode_codecvt(wide, narrow,
-      InputIterator first, InputIterator last, OutputIterator result /*, Error eh*/)
+      InputIterator first, InputIterator last, OutputIterator result,
+      const codecvt_type& ccvt /*, Error eh*/)
     {
       cout << "  wide to narrow" << endl;
     }
@@ -623,148 +674,8 @@ namespace detail
   template <class T>
   constexpr bool fail() { return false; }
 
-  ////  primary template handles the error cases  ----------------------------------------//
-  ////
-  ////  Initial implementatioon only handles narrow to wide, and wide to narrow, so these
-  ////  cases will fail:
-  ////    narrow, narrow
-  ////    narrow, utf8
-  ////    narrow, utf16
-  ////    narrow, utf32
-  ////    uft8, narrow
-  ////    utf16, narrow
-  ////    utf32, narrow
-
-  //template <class FromEncoding, class ToEncoding, class InputIterator,
-  //class OutputIterator, class ErrorHandler = error_handler>
-  //  OutputIterator
-  //  recode(InputIterator, InputIterator, OutputIterator,
-  //    const std::locale&, ErrorHandler)
-  //{
-  //  static_assert(detail::fail<FromEncoding>(), "Invalid template parameters");
-  //}
-
-  //template <class FromEncoding, class ToEncoding, class InputIterator,
-  //class OutputIterator, class ErrorHandler = error_handler>
-  //  OutputIterator
-  //  recode(InputIterator, InputIterator, OutputIterator, ErrorHandler)
-  //{
-  //  static_assert(detail::fail<FromEncoding>(), "Invalid template parameters");
-  //}
-  //  
-  ////  non-converting cases  ------------------------------------------------------------//
-  //
-  ////  narrow to narrow
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //inline OutputIterator recode(narrow, narrow,
-  //  InputIterator first, InputIterator last, OutputIterator result, ErrorHandler)
-  //    { return std::copy(first, last, result); }
-  ////  wide to wide
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //inline OutputIterator recode(narrow, narrow,
-  //  InputIterator first, InputIterator last, OutputIterator result, ErrorHandler)
-  //    { return std::copy(first, last, result); }
-  ////  utf-8 to utf-8
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //inline OutputIterator recode(narrow, narrow,
-  //  InputIterator first, InputIterator last, OutputIterator result, ErrorHandler)
-  //    { return std::copy(first, last, result); }
-  ////  utf16 to utf16
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //inline OutputIterator recode(narrow, narrow,
-  //  InputIterator first, InputIterator last, OutputIterator result, ErrorHandler)
-  //    { return std::copy(first, last, result); }
-  ////  utf32 to utf32
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //inline OutputIterator recode(narrow, narrow,
-  //  InputIterator first, InputIterator last, OutputIterator result, ErrorHandler)
-  //    { return std::copy(first, last, result); }
-
-  ////  converting cases------------------------------------------------------------------//
-
-  ////  narrow to wide
-  //template <class InputIterator, class OutputIterator, class ErrorHandler>
-  //OutputIterator recode(narrow, wide,
-  //  InputIterator first, InputIterator last, OutputIterator result,
-  //  const std::locale& loc, ErrorHandler eh)
-  //{
-  //  constexpr std::size_t buf_size = 20;  // TODO increase this after initial testing
-  //  char in[buf_size];
-  //  const char* in_end = &in[0] + buf_size;
-  //  wchar_t out[buf_size];
-  //  const std::codecvt<wchar_t,char,mbstate_t>& cvt =
-  //    std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t> >(loc);
-  //  std::mbstate_t mbstate  = std::mbstate_t();
-  //  wchar_t* out_next;
-
-  //  while (first != last)
-  //  {
-  //    //  fill the in buffer
-  //    char* in_last = &in[0];
-  //    for (; first != last && in_last != in_end; ++in_last, ++first)
-  //      *in_last = *first;
-
-  //    //  loop until the entire input buffer is processed by the codecvt facet; 
-  //    //  required because the codecvt facet will not process the entire input sequence
-  //    //  when an error occurs or the in buffer ended with only a portion of a multibyte
-  //    //  character.
-  //    char* in_first = &in[0];
-  //    for (; in_first != in_last;)
-  //    {
-  //      //  convert in buffer to out buffer
-  //      const char* in_next;
-  //      std::codecvt_base::result cvt_result
-  //        = cvt.in(mbstate, in, in_last, in_next, out, out + buf_size, out_next);
-
-  //      BOOST_ASSERT(cvt_result != std::codecvt_base::noconv);
-
-  //      if (cvt_result == std::codecvt_base::error)
-  //      {
-  //        //BOOST_ASSERT_MSG(false, "error handling not implemented yet")
-  //        wchar_t placeholder = eh()
-  //      }
-
-  //      //  Note: it is not necessary to further distinguish between an error, partial,
-  //      //  or ok result. The distinctiion is already captured in the mbstate.
-
-  //      //  copy out buffer to result
-  //      for (const wchar_t* itr = &out[0]; itr != out_next; ++itr, ++result)
-  //        *result = *itr;
-
-  //      in_first = in_next;
-  //    }
-
-  //    //  process cvt_result ok, partial, error
-
-  //  }
-
-  //  return result;
-  //}
-
 }  // namespace detail
 
-  //struct error_handler   // default error handler
-  //{
-  //  void operator()() const { throw "barf"; }
-  //};
-
-  //template <class FromEncoding, class ToEncoding, class InputIterator,
-  //  class OutputIterator, class ErrorHandler>
-  //OutputIterator
-  //recode(InputIterator first, InputIterator last, OutputIterator result,
-  //      const std::locale& loc, ErrorHandler eh)
-  //{
-  //  return detail::recode(FromEncoding(), ToEncoding(), first, last, result, loc, eh);
-  //}
-
-  //template <class FromEncoding, class ToEncoding, class InputIterator,
-  //  class OutputIterator, class ErrorHandler>
-  //OutputIterator
-  //recode(InputIterator first, InputIterator last, OutputIterator result,
-  //  ErrorHandler eh)
-  //{
-  //  return recode<FromEncoding, ToEncoding>(first, last, result, std::locale(), eh);
-  //}
 }  // namespace string_encoding
 }  // namespace boost
 
