cf9cf6befc96b904877b76fff5bcd9a6dd555090
Beman <bdawes@acm.org>
2015-07-02 12:08:13 -0400

    Expand Boundary condition tests to cover all of 2.1, 2.2, and 2.3, and to include all possible utf8, utf16, utf32, and wide round trip combinations.

--- test/kuhn_test.cpp
+++ test/kuhn_test.cpp
@@ -15,6 +15,9 @@
 //  This program supplies some of those test cases, normally intended for visual        //
 //  interpretation, in a form suitable for automated regression testing.                //
 //                                                                                      //
+//  Note: Some of the test values have been modified due to changes in the standard,    //
+//  such as 5 and 6 byte UTF-8 sequences no longer being valid.                         //
+//                                                                                      //
 //--------------------------------------------------------------------------------------//
 
 #include <iostream>
@@ -84,27 +87,73 @@ namespace
   {
     cout << "Section 2, Boundary condition test cases" << endl;
 
-    // 2.1  First possible sequence of a certain length
+    //string s2_1_4(u8"\"\U00010000\"");
+    //u32string U2_1_4({U'"', U'\x10000', U'"'});
+    //BOOST_TEST(to_utf32(s2_1_4) == U2_1_4);
+    //cout << s2_1_4.size() << " " << hex_string(s2_1_4) << endl;
+    //cout << U2_1_4.size() << " " << hex_string(U2_1_4) << endl;
 
-    string s2_1_1({'"', '\0', '"'});
-    u32string U2_1_1({U'"', U'\0', U'"'});
-    BOOST_TEST(to_utf32(s2_1_1) == U2_1_1);
+    //  2.1  First possible sequence of a certain length
 
-    string s2_1_2(u8"\"\u0080\"");
-    u32string U2_1_2(U"\"\u0080\"");
-    BOOST_TEST(to_utf32(s2_1_2) == U2_1_2);
+    BOOST_TEST(to_utf32(u8"\"\u0000\"") == U"\"\u0000\"");          // 2.1.1  1 byte
+    BOOST_TEST(to_utf32(u8"\"\u0080\"") == U"\"\u0080\"");          // 2.1.2  2 bytes
+    BOOST_TEST(to_utf32(u8"\"\u0800\"") == U"\"\u0800\"");          // 2.1.3  3 bytes
+    BOOST_TEST(to_utf32(u8"\"\U00010000\"") == U"\"\U00010000\"");  // 2.1.4  4 bytes
 
-    string s2_1_3(u8"\"\u0800\"");
-    u32string U2_1_3(U"\"\u0800\"");
-    BOOST_TEST(to_utf32(s2_1_3) == U2_1_3);
-    cout << s2_1_3.size() << " " << hex_string(s2_1_3) << endl;
-    cout << U2_1_3.size() << " " << hex_string(U2_1_3) << endl;
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\u0000\"")) == u8"\"\u0000\"");
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\u0080\"")) == u8"\"\u0080\"");
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\u0800\"")) == u8"\"\u0800\"");          
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\U00010000\"")) == u8"\"\U00010000\"");
 
-    string s2_1_4(u8"\"\U00010000\"");
-    u32string U2_1_4({U'"', U'\x10000', U'"'});
-    BOOST_TEST(to_utf32(s2_1_4) == U2_1_4);
-    cout << s2_1_4.size() << " " << hex_string(s2_1_4) << endl;
-    cout << U2_1_4.size() << " " << hex_string(U2_1_4) << endl;
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\u0000\"")) == u8"\"\u0000\"");
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\u0080\"")) == u8"\"\u0080\"");
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\u0800\"")) == u8"\"\u0800\"");          
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\U00010000\"")) == u8"\"\U00010000\"");
+
+    BOOST_TEST(to_utf8(to_wide(u8"\"\u0000\"")) == u8"\"\u0000\"");
+    BOOST_TEST(to_utf8(to_wide(u8"\"\u0080\"")) == u8"\"\u0080\"");
+    BOOST_TEST(to_utf8(to_wide(u8"\"\u0800\"")) == u8"\"\u0800\"");          
+    BOOST_TEST(to_utf8(to_wide(u8"\"\U00010000\"")) == u8"\"\U00010000\"");
+
+    //  2.2  Last possible sequence of a certain length
+
+    BOOST_TEST(to_utf32(u8"\"\u007f\"") == U"\"\u007f\"");          // 2.2.1  1 byte
+    BOOST_TEST(to_utf32(u8"\"\u07ff\"") == U"\"\u07ff\"");          // 2.2.2  2 bytes
+    BOOST_TEST(to_utf32(u8"\"\uffff\"") == U"\"\uffff\"");          // 2.2.3  3 bytes
+    BOOST_TEST(to_utf32(u8"\"\U0010FFFF\"") == U"\"\U0010FFFF\"");  // 2.2.4 4b (modified)
+
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\u007f\"")) == u8"\"\u007f\"");        
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\u07ff\"")) == u8"\"\u07ff\"");         
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\uffff\"")) == u8"\"\uffff\"");         
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\U0010FFFF\"")) == u8"\"\U0010FFFF\""); 
+
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\u007f\"")) == u8"\"\u007f\"");        
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\u07ff\"")) == u8"\"\u07ff\"");         
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\uffff\"")) == u8"\"\uffff\"");         
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\U0010FFFF\"")) == u8"\"\U0010FFFF\""); 
+
+    BOOST_TEST(to_utf8(to_wide(u8"\"\u007f\"")) == u8"\"\u007f\"");        
+    BOOST_TEST(to_utf8(to_wide(u8"\"\u07ff\"")) == u8"\"\u07ff\"");         
+    BOOST_TEST(to_utf8(to_wide(u8"\"\uffff\"")) == u8"\"\uffff\"");         
+    BOOST_TEST(to_utf8(to_wide(u8"\"\U0010FFFF\"")) == u8"\"\U0010FFFF\""); 
+
+    //  2.3  Other boundary conditions
+
+    BOOST_TEST(to_utf32(u8"\"\uD7FF\"") == U"\"\uD7FF\"");
+    BOOST_TEST(to_utf32(u8"\"\uE000\"") == U"\"\uE000\"");
+    BOOST_TEST(to_utf32(u8"\"\uFFFD\"") == U"\"\uFFFD\"");
+
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\uD7FF\"")) == u8"\"\uD7FF\"");
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\uE000\"")) == u8"\"\uE000\"");
+    BOOST_TEST(to_utf8(to_utf32(u8"\"\uFFFD\"")) == u8"\"\uFFFD\"");
+
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\uD7FF\"")) == u8"\"\uD7FF\"");
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\uE000\"")) == u8"\"\uE000\"");
+    BOOST_TEST(to_utf8(to_utf16(u8"\"\uFFFD\"")) == u8"\"\uFFFD\"");
+
+    BOOST_TEST(to_utf8(to_wide(u8"\"\uD7FF\"")) == u8"\"\uD7FF\"");
+    BOOST_TEST(to_utf8(to_wide(u8"\"\uE000\"")) == u8"\"\uE000\"");
+    BOOST_TEST(to_utf8(to_wide(u8"\"\uFFFD\"")) == u8"\"\uFFFD\"");
 
     cout << "  Boundary condition test cases done" << endl;
   }
