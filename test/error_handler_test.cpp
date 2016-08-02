54ad5045620f573371b46c2759a5d91850becfa5
Beman <bdawes@acm.org>
2016-08-02 07:54:39 -0400

    Oops! Forgot to remove the inline versions of hex_string, etc.

--- test/error_handler_test.cpp
+++ test/error_handler_test.cpp
@@ -48,37 +48,6 @@ namespace
   template<> struct underlying<wchar_t> { typedef boost::uint_least16_t type; };
 #endif
 
-  template <class T>
-  std::string to_hex(T x)
-  {
-    const char hex[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
-    std::string tmp;
-    typename underlying<T>::type big_x
-      = boost::endian::native_to_big(static_cast<typename underlying<T>::type>(x));
-
-    const unsigned char* p = reinterpret_cast<const unsigned char*>(&big_x);
-    const unsigned char* e = p + sizeof(T);
-
-    for (; p < e; ++p)
-    {
-      tmp += hex[*p >> 4];    // high-order nibble
-      tmp += hex[*p & 0x0f];  // low-order nibble
-    }
-    return tmp;
-  }
-
-  template <class String>
-  std::string hex_string(const String& s)
-  {
-    std::string tmp;
-    for (auto x : s)
-    {
-      tmp += " 0x";
-      tmp += to_hex(x);
-    }
-    return tmp;
-  }
-
 //void invalid_utf8_characters()
 //  {
 //    cout << "invalid_utf8_characters" << endl;
--- test/smoke_test.cpp
+++ test/smoke_test.cpp
@@ -28,6 +28,7 @@ using std::endl;
 #endif
 
 using namespace boost::unicode;
+using namespace boost::unicode::detail;
 using std::string;
 using std::wstring;
 using std::u16string;
@@ -49,37 +50,6 @@ namespace
 #else
   template<> struct underlying<wchar_t> { typedef boost::uint_least16_t type; };
 #endif
-
-  template <class T>
-  std::string to_hex(T x)
-  {
-    const char hex[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
-    std::string tmp;
-    typename underlying<T>::type big_x
-      = boost::endian::native_to_big(static_cast<typename underlying<T>::type>(x));
-
-    const unsigned char* p = reinterpret_cast<const unsigned char*>(&big_x);
-    const unsigned char* e = p + sizeof(T);
-
-    for (; p < e; ++p)
-    {
-      tmp += hex[*p >> 4];    // high-order nibble
-      tmp += hex[*p & 0x0f];  // low-order nibble
-    }
-    return tmp;
-  }
-
-  template <class String>
-  std::string hex_string(const String& s)
-  {
-    std::string tmp;
-    for (auto x : s)
-    {
-      tmp += " 0x";
-      tmp += to_hex(x);
-    }
-    return tmp;
-  }
 
   template <class T>
   void check_inserter(T x, const string& expected)
