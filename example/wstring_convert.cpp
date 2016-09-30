e2ea33ab11564c4c89aeca73101c9260767cfd3e
Beman <bdawes@acm.org>
2016-09-29 20:32:27 -0400

    Update docs, wstring_convert example.

--- example/wstring_convert.cpp
+++ example/wstring_convert.cpp
@@ -27,8 +27,10 @@ int main()
   std::string read();                      // read Big-5 string from database 1
   void write(const std::string& data);     // write Shift-JIS string to database 2
 
-  std::wstring_convert<stdext::cvt::codecvt_sjis<char32_t>> sjis;
-  std::wstring_convert<stdext::cvt::codecvt_big5<char32_t>> big5;
+  //std::wstring_convert<stdext::cvt::codecvt_sjis<char32_t>> sjis;
+  //std::wstring_convert<stdext::cvt::codecvt_big5<char32_t>> big5;
+  std::wstring_convert<stdext::cvt::codecvt_sjis<wchar_t>> sjis;
+  std::wstring_convert<stdext::cvt::codecvt_big5<wchar_t>> big5;
 
   write(sjis.to_bytes(big5.from_bytes(read())));
 
