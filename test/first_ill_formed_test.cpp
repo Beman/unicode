bbe6b41976f80ea7582b03786d29d493cfb2018c
Beman <bdawes@acm.org>
2016-10-03 06:25:36 -0400

    Redesign test to compare against a pedantic 10646-based implementation

--- test/first_ill_formed_test.cpp
+++ test/first_ill_formed_test.cpp
@@ -14,6 +14,7 @@ using std::endl;
 #include <string>
 #include <cstring>
 #include <iterator>
+//#define BOOST_LIGHTWEIGHT_TEST_OSTREAM std::cout
 #include <boost/core/lightweight_test.hpp>
 #include <boost/detail/lightweight_main.hpp>
 
@@ -26,45 +27,158 @@ using std::u32string;
 
 namespace
 {
+
+/*
+  ISO/IEC 10646:2014 9.4 says "Table 3 lists all the ranges (inclusive) of the octet
+  sequences that are well-formed in UTF-8. Any UTF-8 sequence that does not match the
+  patterns listed in table 3 is ill-formed." and also says "As a consequence of the
+  well-formedness conditions specified in table 9.2 [sic], the following octet values
+  are disallowed in UTF-8: C0-C1, F5-FE."
+  
+  pedantic_first_ill_formed() is pedantic implmentation of those rules.
+*/
+
+  inline
+  std::pair<const char*, const char*>
+    pedantic_first_ill_formed(const char* first, const char* last) BOOST_NOEXCEPT
+  {
+    const char* first_code_unit;
+    for (; first != last;) // each code point
+    {
+      first_code_unit = first;
+      unsigned octet = static_cast<unsigned char>(*first++);
+
+      if (octet <= 0x7Fu)
+        continue;
+      else if (octet >= 0xC2u && octet <= 0xDFu)  // two octets
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0x80u && octet <= 0xBFu)  // octet two
+          continue;
+      }
+      else if (octet == 0xE0u)  // three octets case one
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0xA0u && octet <= 0xBFu)  // octet two
+        {
+          if (first == last)
+            break;
+          octet = static_cast<unsigned char>(*first++);
+          if (octet >= 0x80u && octet <= 0xBFu)  // octet three
+            continue;
+        }
+      }
+      else if ((octet >= 0xE1u && octet <= 0xECu)
+        || (octet >= 0xEEu && octet <= 0xEFu))  // three octets case two
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0x80u && octet <= 0xBFu)  // octet two
+        {
+          if (first == last)
+            break;
+          octet = static_cast<unsigned char>(*first++);
+          if (octet >= 0x80u && octet <= 0xBFu)  // octet three
+            continue;
+        }
+      }
+      else if (octet == 0xEDu)  // three octets case three
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0x80u && octet <= 0x9Fu)  // octet two
+        {
+          if (first == last)
+            break;
+          octet = static_cast<unsigned char>(*first++);
+          if (octet >= 0x80u && octet <= 0xBFu)  // octet three
+            continue;
+        }
+      }
+      else if (octet == 0xF0u)  // four octets case one
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0x90u && octet <= 0xBFu)  // octet two
+        {
+          if (first == last)
+            break;
+          octet = static_cast<unsigned char>(*first++);
+          if (octet >= 0x80u && octet <= 0xBFu)  // octet three
+          {
+            if (first == last)
+              break;
+            octet = static_cast<unsigned char>(*first++);
+            if (octet >= 0x80u && octet <= 0xBFu)  // octet four
+              continue;
+          }
+        }
+      }
+      else if (octet >= 0xF1u && octet <= 0xF4u)  // four octets case two
+      {
+        if (first == last)
+          break;
+        octet = static_cast<unsigned char>(*first++);
+        if (octet >= 0x80u && octet <= 0xBFu)  // octet two
+        {
+          if (first == last)
+            break;
+          octet = static_cast<unsigned char>(*first++);
+          if (octet >= 0x80u && octet <= 0xBFu)  // octet three
+          {
+            if (first == last)
+              break;
+            octet = static_cast<unsigned char>(*first++);
+            if (octet >= 0x80u && octet <= 0xBFu)  // octet four
+              continue;
+          }
+        }
+      }
+      return std::make_pair(first_code_unit, first);   // failure
+    }
+    return std::make_pair(last, last);                 // success
+  }
+
   void utf8_test()
   {
     cout << "start utf8_test" << endl;
-/*
-  ISO/IEC 10646:2014 9.4 "As a consequence of the well-formedness conditions specified
-  in table 9.2, the following octet values are disallowed in UTF-8: C0-C1, F5-FE."
 
-  The idea of this test is to add additional logic to first_ill_formed() to detect those
-  values, then test all possible values of one to four octets with and without the new
-  logic enabled. If the tests find a different number of ill-formed sequences, then the
-  new logic is necessary. Otherwise, the disallowed octet values are already disallow by
-  the existing logic.
-*/
-    union
+    for (uint32_t i = 0;;)
     {
-      uint32_t int_;
-      char c_[4];
-    } tmp;
+      auto pedantic = pedantic_first_ill_formed(reinterpret_cast<const char *>(&i),
+        reinterpret_cast<const char *>(&i) + 4);
+      auto normal = boost::unicode::first_ill_formed(reinterpret_cast<const char *>(&i),
+          reinterpret_cast<const char *>(&i)+4);
+      //BOOST_TEST(pedantic == normal);
+      if (pedantic != normal)
+      {
+        ++boost::detail::test_errors();
+        cout << "    Error:  first     second   for "
+             << hex_string(string(reinterpret_cast<const char *>(&i),
+        reinterpret_cast<const char *>(&i) + 4)) << " at "
+          << reinterpret_cast<void*>(&i) <<
+          "\n pedantic:  " << (void*)pedantic.first << "  "
+            << (void*)pedantic.second << '\n' <<
+          "   normal:  " << (void*)normal.first << "  "
+            << (void*)normal.second << '\n';
+      }
+
+      if (boost::detail::test_errors() >= 10)
+      {
+        cout << "  maximum errors exceeded, test cancelled" << endl;
+        break;
+      }
+      ++i;
+    };
 
 
-    uint32_t ill_formed_count = 0;
-
-    for (uint32_t value = 1u; value <= 0x10FFFFu; ++value)
-    {
-      string str;
-      tmp.int_ = boost::endian::native_to_big(value);
-      if (tmp.c_[0] != '\0')
-        str.append(&tmp.c_[0], 4);
-      else if (tmp.c_[1] != '\0')
-        str.append(&tmp.c_[1], 3);
-      else if (tmp.c_[2] != '\0')
-        str.append(&tmp.c_[2], 2);
-      else
-        str.append(&tmp.c_[3], 1);
-      if (!boost::unicode::is_well_formed(str))
-        ++ill_formed_count;
-    }
-
-    cout << "    ill-formed count is " << ill_formed_count << endl;
 
     cout << "  end utf8_test" << endl;
 
